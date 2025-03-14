43406fe38b61fe17346e9a52dc6ee32b78a5df96
Navanita Das <134119127+NAVANITAA@users.noreply.github.com>
2025-03-14 20:08:26 +0530

    Update my_first_dbt_model.sql

--- my_dbt_project/models/example/my_first_dbt_model.sql
+++ my_dbt_project/models/example/my_first_dbt_model.sql
@@ -1,27 +1,4 @@
-WITH source_data AS (
-    SELECT
-        Segment,
-        Country,
-        Product,
-        "Discount Band" AS discount_band
-    FROM {{ source('google_sheets', 'navanita_fivetran_test') }}
-),
-
-calculated_metrics AS (
-    SELECT
-        *,
-        "navanita" as emp_name,
-        "das" as emp_surname
-    FROM source_data
-)
-
-SELECT
-    Segment,
-    Country,
-    Product,
-    discount_band,
-    emp_name,
-    emp_surname
-FROM calculated_metrics
-WHERE emp_name = "navanita"
-ORDER BY order_date;
+SELECT 
+    *,
+    CURRENT_TIMESTAMP AS data_loaded_time
+FROM navanita_dummy_data.dummy_data;

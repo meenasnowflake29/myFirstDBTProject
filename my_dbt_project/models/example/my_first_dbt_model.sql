f4ecae3b526cd72d59c0ea24fe95dba4eb841661
Navanita Das <134119127+NAVANITAA@users.noreply.github.com>
2025-03-17 15:26:00 +0530

    Update my_first_dbt_model.sql

--- my_dbt_project/models/example/my_first_dbt_model.sql
+++ my_dbt_project/models/example/my_first_dbt_model.sql
@@ -1,4 +1,9 @@
 SELECT 
-    *,
+    Country,
+    Product,
+    TRY_CAST("Units Sold" AS DECIMAL(18,2)) AS units_sold,
+    TRY_CAST(REPLACE("Sale Price", '$', '') AS DECIMAL(18,2)) AS sale_price,
+    TRY_CAST("Units Sold" AS DECIMAL(18,2)) * TRY_CAST(REPLACE("Sale Price", '$', '') AS DECIMAL(18,2)) AS total_revenue,
+    TO_DATE("Date", 'MM/DD/YYYY') AS sale_date,
     CURRENT_TIMESTAMP AS data_loaded_time_tm
-FROM LOGAN_DATA.navanita_dummy_data.dummy_data
+FROM LOGAN_DATA.TALEND.navanita_fivetran_test;

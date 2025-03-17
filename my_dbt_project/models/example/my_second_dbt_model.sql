2412ed5d6ae588ddd17ae52d0d8aa677ca32a918
rohithj1999 <rohith9uk@gmail.com>
2025-03-17 18:31:56 +0530

    Update my_second_dbt_model.sql

--- my_dbt_project/models/example/my_second_dbt_model.sql
+++ my_dbt_project/models/example/my_second_dbt_model.sql
@@ -1,9 +1,4 @@
 SELECT 
-    Country,
-    Product,
-    TRY_CAST("Units Sold" AS DECIMAL(18,2)) AS units_sold,
-    TRY_CAST(REPLACE("Sale Price", '$', '') AS DECIMAL(18,2)) AS sale_price,
-    TRY_CAST("Units Sold" AS DECIMAL(18,2)) * TRY_CAST(REPLACE("Sale Price", '$', '') AS DECIMAL(18,2)) AS total_revenue,
-    TO_DATE("Date", 'MM/DD/YYYY') AS sale_date,
-    CURRENT_TIMESTAMP AS data_loaded_time_tm
-FROM LOGAN_DATA.TALEND.navanita_fivetran_test;
+    *,
+    CURRENT_TIMESTAMP AS data_loaded_time  -- Adds a new column
+FROM LOGAN_DATA.GOSHEET.GOTABLE;

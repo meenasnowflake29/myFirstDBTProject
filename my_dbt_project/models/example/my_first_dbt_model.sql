2663d19a1c938e4f943c41fd6d0a901f6d53cbec
Navanita Das <134119127+NAVANITAA@users.noreply.github.com>
2025-03-17 18:54:48 +0530

    Update my_first_dbt_model.sql

--- my_dbt_project/models/example/my_first_dbt_model.sql
+++ my_dbt_project/models/example/my_first_dbt_model.sql
@@ -1,4 +1,10 @@
-SELECT Country, SUM(Profit) AS Total_Profit
+-- SELECT Country, SUM(Profit) AS Total_Profit
+-- FROM LOGAN_DATA.navanita_dummy_data.dummy_data
+-- GROUP BY Country
+-- ORDER BY Total_Profit DESC
+
+SELECT Country,
+       SUM(CASE WHEN Product = 'Carretera' THEN Sales ELSE 0 END) AS Carretera_Sales,
+       SUM(CASE WHEN Product = 'Montana' THEN Sales ELSE 0 END) AS Montana_Sales
 FROM LOGAN_DATA.navanita_dummy_data.dummy_data
-GROUP BY Country
-ORDER BY Total_Profit DESC
+GROUP BY Country;

129df7165aea4f8b3079f285e947ffa9795667f9
Navanita Das <134119127+NAVANITAA@users.noreply.github.com>
2025-03-17 15:50:20 +0530

    Update my_first_dbt_model.sql

--- my_dbt_project/models/example/my_first_dbt_model.sql
+++ my_dbt_project/models/example/my_first_dbt_model.sql
@@ -1,4 +1,8 @@
 SELECT 
-    *,
-    CURRENT_TIMESTAMP AS data_loaded_time_tm
+    Country, 
+    Product, 
+    "Units Sold", 
+    "Sale Price", 
+    "Profit", 
+    "Year" + 1 AS next_year
 FROM LOGAN_DATA.navanita_dummy_data.dummy_data

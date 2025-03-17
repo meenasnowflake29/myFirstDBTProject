3d7f154ab47f1dc9b5cccc1aa79e36754d2c9a8f
Navanita Das <134119127+NAVANITAA@users.noreply.github.com>
2025-03-17 15:53:51 +0530

    Update my_first_dbt_model.sql

--- my_dbt_project/models/example/my_first_dbt_model.sql
+++ my_dbt_project/models/example/my_first_dbt_model.sql
@@ -1,8 +1,5 @@
 SELECT 
     Country, 
-    Product, 
-    "Units Sold", 
-    "Sale Price", 
-    "Profit", 
-    "Year" + 1 AS next_year
+    Product,
+    'Year' + 1 AS next_year
 FROM LOGAN_DATA.navanita_dummy_data.dummy_data

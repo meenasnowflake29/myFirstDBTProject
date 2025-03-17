fefef3fc0736d02f05d3b55cadbfffb3ed581c3a
Navanita Das <134119127+NAVANITAA@users.noreply.github.com>
2025-03-17 21:02:23 +0530

    Update my_first_dbt_model.sql

--- my_dbt_project/models/example/my_first_dbt_model.sql
+++ my_dbt_project/models/example/my_first_dbt_model.sql
@@ -1,35 +1,3 @@
--- 
--- SELECT Country, SUM(Profit) AS Total_Profit
--- FROM LOGAN_DATA.navanita_dummy_data.dummy_data
--- GROUP BY Country
--- ORDER BY Total_Profit DESC
-
--- SELECT Year, 
---        SUM(Sales) AS Total_Sales, 
---        LAG(SUM(Sales)) OVER (ORDER BY Year) AS Previous_Year_Sales,
---        (SUM(Sales) - LAG(SUM(Sales)) OVER (ORDER BY Year)) / LAG(SUM(Sales)) OVER (ORDER BY Year) * 100 AS YoY_Growth
--- FROM sales_data
--- GROUP BY Year
--- ORDER BY Year
-
--- WITH YearlySales AS (
---     SELECT Year, Country, SUM(Sales) AS Country_Sales,
---            SUM(SUM(Sales)) OVER (PARTITION BY Year) AS Total_Sales
---     FROM LOGAN_DATA.navanita_dummy_data.dummy_data
---     GROUP BY Year, Country
--- )
--- SELECT Year, Country, Country_Sales, 
---        ROUND((Country_Sales * 100.0) / Total_Sales, 2) AS Sales_Contribution
--- FROM YearlySales
--- ORDER BY Year, Sales_Contribution DESC
-
--- SELECT Year, Country, 
---        SUM(Sales) AS Country_Sales, 
---        ROUND((SUM(Sales) * 100.0) / SUM(SUM(Sales)) OVER (PARTITION BY Year), 2) AS Sales_Contribution
--- FROM LOGAN_DATA.navanita_dummy_data.dummy_data
--- GROUP BY Year, Country
--- ORDER BY Year, Sales_Contribution DESC
-
 SELECT 
     *,
     CURRENT_TIMESTAMP AS data_loaded_time

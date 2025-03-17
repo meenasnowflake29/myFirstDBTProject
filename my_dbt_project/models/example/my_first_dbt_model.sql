00d0a87ef2b18c78a8069928f051db1feaf2a701
Navanita Das <134119127+NAVANITAA@users.noreply.github.com>
2025-03-17 20:50:21 +0530

    Update my_first_dbt_model.sql

--- my_dbt_project/models/example/my_first_dbt_model.sql
+++ my_dbt_project/models/example/my_first_dbt_model.sql
@@ -12,13 +12,20 @@
 -- GROUP BY Year
 -- ORDER BY Year
 
-WITH YearlySales AS (
-    SELECT Year, Country, SUM(Sales) AS Country_Sales,
-           SUM(SUM(Sales)) OVER (PARTITION BY Year) AS Total_Sales
-    FROM LOGAN_DATA.navanita_dummy_data.dummy_data
-    GROUP BY Year, Country
-)
-SELECT Year, Country, Country_Sales, 
-       ROUND((Country_Sales * 100.0) / Total_Sales, 2) AS Sales_Contribution
-FROM YearlySales
+-- WITH YearlySales AS (
+--     SELECT Year, Country, SUM(Sales) AS Country_Sales,
+--            SUM(SUM(Sales)) OVER (PARTITION BY Year) AS Total_Sales
+--     FROM LOGAN_DATA.navanita_dummy_data.dummy_data
+--     GROUP BY Year, Country
+-- )
+-- SELECT Year, Country, Country_Sales, 
+--        ROUND((Country_Sales * 100.0) / Total_Sales, 2) AS Sales_Contribution
+-- FROM YearlySales
+-- ORDER BY Year, Sales_Contribution DESC
+
+SELECT Year, Country, 
+       SUM(Sales) AS Country_Sales, 
+       ROUND((SUM(Sales) * 100.0) / SUM(SUM(Sales)) OVER (PARTITION BY Year), 2) AS Sales_Contribution
+FROM LOGAN_DATA.navanita_dummy_data.dummy_data
+GROUP BY Year, Country
 ORDER BY Year, Sales_Contribution DESC

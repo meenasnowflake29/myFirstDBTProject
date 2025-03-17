ba54239bcef890b168e55fa88579879d19b1075d
Navanita Das <134119127+NAVANITAA@users.noreply.github.com>
2025-03-17 21:05:13 +0530

    Update my_first_dbt_model.sql

--- my_dbt_project/models/example/my_first_dbt_model.sql
+++ my_dbt_project/models/example/my_first_dbt_model.sql
@@ -1,4 +1,6 @@
-SELECT 
-    *,
-    CURRENT_TIMESTAMP AS data_loaded_time
+SELECT Year, Country, 
+       SUM(Sales) AS Country_Sales, 
+       ROUND((SUM(Sales) * 100.0) / SUM(SUM(Sales)) OVER (PARTITION BY Year), 2) AS Sales_Contribution
 FROM LOGAN_DATA.navanita_dummy_data.dummy_data
+GROUP BY Year, Country
+ORDER BY Year, Sales_Contribution DESC

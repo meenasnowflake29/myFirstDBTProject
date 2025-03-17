54b28129d1cd5a32033ae9503feca5a75992874a
Navanita Das <134119127+NAVANITAA@users.noreply.github.com>
2025-03-17 19:05:14 +0530

    Update my_first_dbt_model.sql

--- my_dbt_project/models/example/my_first_dbt_model.sql
+++ my_dbt_project/models/example/my_first_dbt_model.sql
@@ -1,3 +1,4 @@
+-- 
 -- SELECT Country, SUM(Profit) AS Total_Profit
 -- FROM LOGAN_DATA.navanita_dummy_data.dummy_data
 -- GROUP BY Country
@@ -7,4 +8,12 @@ SELECT Country,
        SUM(CASE WHEN Product = 'Carretera' THEN Sales ELSE 0 END) AS Carretera_Sales,
        SUM(CASE WHEN Product = 'Montana' THEN Sales ELSE 0 END) AS Montana_Sales
 FROM LOGAN_DATA.navanita_dummy_data.dummy_data
-GROUP BY Country;
+GROUP BY Country
+
+-- SELECT Year, 
+--        SUM(Sales) AS Total_Sales, 
+--        LAG(SUM(Sales)) OVER (ORDER BY Year) AS Previous_Year_Sales,
+--        (SUM(Sales) - LAG(SUM(Sales)) OVER (ORDER BY Year)) / LAG(SUM(Sales)) OVER (ORDER BY Year) * 100 AS YoY_Growth
+-- FROM sales_data
+-- GROUP BY Year
+-- ORDER BY Year;

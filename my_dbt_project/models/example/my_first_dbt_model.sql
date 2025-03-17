195c48d62db20effbc3a992dd510e1752caab960
Navanita Das <134119127+NAVANITAA@users.noreply.github.com>
2025-03-17 20:35:51 +0530

    Update my_first_dbt_model.sql

--- my_dbt_project/models/example/my_first_dbt_model.sql
+++ my_dbt_project/models/example/my_first_dbt_model.sql
@@ -4,10 +4,21 @@
 -- GROUP BY Country
 -- ORDER BY Total_Profit DESC
 
-SELECT Year, 
-       SUM(Sales) AS Total_Sales, 
-       LAG(SUM(Sales)) OVER (ORDER BY Year) AS Previous_Year_Sales,
-       (SUM(Sales) - LAG(SUM(Sales)) OVER (ORDER BY Year)) / LAG(SUM(Sales)) OVER (ORDER BY Year) * 100 AS YoY_Growth
-FROM sales_data
-GROUP BY Year
-ORDER BY Year
+-- SELECT Year, 
+--        SUM(Sales) AS Total_Sales, 
+--        LAG(SUM(Sales)) OVER (ORDER BY Year) AS Previous_Year_Sales,
+--        (SUM(Sales) - LAG(SUM(Sales)) OVER (ORDER BY Year)) / LAG(SUM(Sales)) OVER (ORDER BY Year) * 100 AS YoY_Growth
+-- FROM sales_data
+-- GROUP BY Year
+-- ORDER BY Year
+
+WITH YearlySales AS (
+    SELECT Year, Country, SUM(Sales) AS Country_Sales,
+           SUM(SUM(Sales)) OVER (PARTITION BY Year) AS Total_Sales
+    FROM sales_data
+    GROUP BY Year, Country
+)
+SELECT Year, Country, Country_Sales, 
+       ROUND((Country_Sales * 100.0) / Total_Sales, 2) AS Sales_Contribution
+FROM YearlySales
+ORDER BY Year, Sales_Contribution DESC

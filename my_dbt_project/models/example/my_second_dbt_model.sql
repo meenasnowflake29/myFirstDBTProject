0147fc0fb3db9e47d8baee336a834a72f9d234e7
Navanita Das <134119127+NAVANITAA@users.noreply.github.com>
2025-03-17 15:23:14 +0530

    Update my_second_dbt_model.sql

--- my_dbt_project/models/example/my_second_dbt_model.sql
+++ my_dbt_project/models/example/my_second_dbt_model.sql
@@ -1,24 +1,9 @@
-WITH filtered_sales AS (
-    SELECT 
-        Country,
-        Product,
-        SUM(Units_Sold) AS total_units_sold,
-        AVG(Sale_Price) AS avg_sale_price,
-        SUM(Sales) AS total_sales,
-        SUM(Profit) AS total_profit,
-        CURRENT_TIMESTAMP AS data_loaded_time_tm
-    FROM {{ ref('dummy_data') }}  -- Using dbt ref function for table reference
-    WHERE Segment = 'Government'  -- Filtering only 'Government' segment
-    GROUP BY Country, Product
-)
-
 SELECT 
     Country,
     Product,
-    total_units_sold,
-    avg_sale_price,
-    total_sales,
-    total_profit,
-    ROUND((total_profit / NULLIF(total_sales, 0)) * 100, 2) AS profit_margin -- Calculating profit %
-FROM filtered_sales
-ORDER BY total_profit DESC;
+    TRY_CAST("Units Sold" AS DECIMAL(18,2)) AS units_sold,
+    TRY_CAST(REPLACE("Sale Price", '$', '') AS DECIMAL(18,2)) AS sale_price,
+    TRY_CAST("Units Sold" AS DECIMAL(18,2)) * TRY_CAST(REPLACE("Sale Price", '$', '') AS DECIMAL(18,2)) AS total_revenue,
+    TO_DATE("Date", 'MM/DD/YYYY') AS sale_date,
+    CURRENT_TIMESTAMP AS data_loaded_time_tm
+FROM LOGAN_DATA.TALEND.navanita_fivetran_test;

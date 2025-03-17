39412087277692b3abfa6b170c9015466e050f14
rohithj1999 <rohith9uk@gmail.com>
2025-03-17 19:32:57 +0530

    Update my_second_dbt_model.sql

--- my_dbt_project/models/example/my_second_dbt_model.sql
+++ my_dbt_project/models/example/my_second_dbt_model.sql
@@ -1,9 +1,9 @@
 {{ config(materialized='table') }}
 
 SELECT 
-    ROW_NUMBER() OVER (ORDER BY major ASC) AS student_id, 
-    student_name, 
-    class_level, 
-    major
+    ROW_NUMBER() OVER (ORDER BY "Major" ASC) AS student_id, 
+    "Student Name" AS student_name, 
+    "Class Level" AS class_level, 
+    "Major" AS major
 FROM LOGAN_DATA.GOSHEET.GOTABLE
-ORDER BY major ASC
+ORDER BY "Major" ASC

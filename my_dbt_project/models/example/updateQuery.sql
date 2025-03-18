b41afc6f371a4fe2337bc823ad96d5d6af713516
meenasnowflake29 <meena.snowflake29@gmail.com>
2025-03-18 20:36:24 +0530

    Update updateQuery.sql

--- my_dbt_project/models/example/updateQuery.sql
+++ my_dbt_project/models/example/updateQuery.sql
@@ -1,3 +1,22 @@
-UPDATE LOGAN_DATA.MEENA_SAMPLE.MEENA_SAMPLEDATA 
-SET CUSTOMER_NAME = 'Jane Smith' 
-WHERE CUSTOMER_ID = 'ZD-21925'
+{{ config(
+    materialized='incremental',
+    unique_key='ROW_ID'
+) }}
+
+SELECT 
+    _ROW, 
+    CUSTOMER_ID, 
+    ORDER_ID, 
+    SHIP_DATE, 
+    SHIP_MODE, 
+    COUNTRY, 
+    ORDER_DATE, 
+    CUSTOMER_NAME, 
+    SEGMENT
+FROM {{ source('logandata', 'google_sheets_meena_trans') }}
+
+{% if is_incremental() %}
+-- Only insert new or updated records
+WHERE _FIVETRAN_SYNCED > (SELECT MAX(_FIVETRAN_SYNCED) FROM {{ this }})
+{% endif %}
+

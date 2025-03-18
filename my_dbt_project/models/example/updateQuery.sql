3e9cae42a39dc2baa94f59797b3795897277d090
meenasnowflake29 <meena.snowflake29@gmail.com>
2025-03-18 20:41:12 +0530

    Update updateQuery.sql

--- my_dbt_project/models/example/updateQuery.sql
+++ my_dbt_project/models/example/updateQuery.sql
@@ -1,22 +1,5 @@
-{{ config(
-    materialized='incremental',
-    unique_key='ROW_ID'
-) }}
-
 SELECT 
-    _ROW, 
-    CUSTOMER_ID, 
-    ORDER_ID, 
-    SHIP_DATE, 
-    SHIP_MODE, 
-    COUNTRY, 
-    ORDER_DATE, 
-    CUSTOMER_NAME, 
-    SEGMENT
-FROM {{ source('logandata', 'google_sheets_meena_trans') }}
-
-{% if is_incremental() %}
--- Only insert new or updated records
-WHERE _FIVETRAN_SYNCED > (SELECT MAX(_FIVETRAN_SYNCED) FROM {{ this }})
-{% endif %}
+    *,
+    CURRENT_TIMESTAMP AS data_loaded_time  -- Adds a new column
+FROM LOGAN_DATA.MEENA_SAMPLE.MEENA_SAMPLEDATA
 
